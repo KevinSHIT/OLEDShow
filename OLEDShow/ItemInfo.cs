fb2232b7bda5a272cd2dfdc0fa62c26b069092f4
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-03-14 21:30:05 +0000

    seperate InfoText

---
+++ OLEDShow/ItemInfo.cs
@@ -0,0 +1,37 @@
+﻿namespace OLEDShow
+{
+    public class InfoText
+    {
+        string _time;
+        public string Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+                RefreshText();
+            }
+        }
+
+        string _network;
+        public string Network
+        {
+            get => _network;
+            set
+            {
+                _network = value;
+                RefreshText();
+            }
+        }
+
+        public override string ToString()
+        {
+            return Time + "\n" + Network;
+        }
+
+        public void RefreshText()
+        {
+            Shared.MainActivity.SetTxvText(this.ToString());
+        }
+    }
+}
--- OLEDShow/Shared.cs
+++ OLEDShow/Shared.cs
@@ -5,39 +5,4 @@
         public static MainActivity MainActivity;
         public static InfoText InfoText = new InfoText();
     }
-
-    public class InfoText
-    {
-        string _time;
-        public string Time
-        {
-            get => _time;
-            set
-            {
-                _time = value;
-                RefreshText();
-            }
-        }
-
-        string _network;
-        public string Network
-        {
-            get => _network;
-            set
-            {
-                _network = value;
-                RefreshText();
-            }
-        }
-
-        public override string ToString()
-        {
-            return Time + "\n" + Network;
-        }
-
-        public void RefreshText()
-        {
-            Shared.MainActivity.SetTxvText(this.ToString());
-        }
-    }
 }
