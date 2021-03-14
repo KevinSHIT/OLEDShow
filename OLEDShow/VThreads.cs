6ea8963c986b82bb86c9ce13461767c08f412049
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-03-14 21:24:57 +0000

    add VThread

---
+++ OLEDShow/VThreads.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+
+namespace OLEDShow
+{
+    public class VThreads
+    {
+        private Thread _thread;
+        private ThreadStart _act;
+
+        public VThreads(ThreadStart act)
+        {
+            _act = act;
+        }
+
+        public void Start(bool abord = true)
+        {
+            if (abord)
+                Stop();
+
+            _thread = new Thread(_act);
+            _thread.Start();
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                _thread.Abort();
+            }
+            catch { }
+        }
+
+
+    }
+}
